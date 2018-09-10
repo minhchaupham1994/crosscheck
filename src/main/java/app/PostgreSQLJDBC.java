c7282368ef0e97b3fc849548778a069752efd940
minhchaupham1994 <phamducminhchau@gmail.com>
2018-09-10 17:26:21 +0700

    Setup DB connection

---
+++ src/main/java/app/PostgreSQLJDBC.java
@@ -0,0 +1,32 @@
+package app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class PostgreSQLJDBC {
+	static final String url = "jdbc:postgresql://localhost:5432/maas_db";
+	static final String user = "postgres";
+	static final String password = "1278";
+    
+    public static ResultSet execute(String query) {
+    	ResultSet rs = null;
+    	
+    	
+    	try {
+			Connection con = DriverManager.getConnection(url, user, password);
+			Statement st = con.createStatement();
+			rs = st.executeQuery(query);
+			
+    	} catch (SQLException ex) {
+    		System.out.println(ex);
+		    
+		}
+    	return rs;
+    }
+   
+}
